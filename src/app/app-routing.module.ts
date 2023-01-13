fb83fb28147aea5d506a54d5ee490d77ef4a7ae2
Shen Sabado <stephanieshen.sabado@gmail.com>
2023-01-13 13:22:53 +0800

    intital setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'product-list'
+  },
+  {
+    path: 'product-list',
+    loadChildren: () => import('src/app/features/product-list/product-list.module').then(productList => productList.ProductListModule)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'gori';
+export class AppComponent implements OnInit {
+  
+  ngOnInit(): void {
+    this.onWindowResize();
+  }
+
+  @HostListener('window:orientationchange')
+  @HostListener('window:resize')
+  onWindowResize(): void {
+      document.documentElement.style.setProperty(
+        '--app-height',
+        `${window.innerHeight}px`
+      );
+
+      document.documentElement.style.setProperty(
+        '--base-font-size',
+        `${window.innerHeight / 45}px`
+    );
+  }
 }
+
+
+/**
+ * Item name
+ * Brand
+ * Price
+ * Quantity
+ * 
+ */
